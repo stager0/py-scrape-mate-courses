93cd2978009b3ef242eacecb3f40e5d81167a94b
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-01 22:38:52 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,10 @@
+import requests
+
+from bs4 import BeautifulSoup, Tag
 from dataclasses import dataclass
+
+
+URL_TO_SCRAPING = "https://mate.academy"
 
 
 @dataclass
@@ -8,5 +14,31 @@ class Course:
     duration: str
 
 
+def get_list_of_courses(soup: list[Tag]) -> list[Course]:
+    courses_data = []
+
+    for block in soup:
+        course_duration = block.select("p")[0].get_text()
+        course_name = block.select_one("h3").get_text()
+        course_description = block.select("p")[1].get_text()
+        courses_data.append(Course(
+            name=course_name,
+            short_description=course_description,
+            duration=course_duration)
+        )
+
+    return courses_data
+
+
 def get_all_courses() -> list[Course]:
-    pass
+    text = requests.get(URL_TO_SCRAPING).content
+    soup = BeautifulSoup(text, "html.parser")
+
+    blocks = soup.select(".ProfessionCard_content__mPiVi")
+
+    courses = get_list_of_courses(blocks)
+    return courses
+
+
+if __name__ == "__main__":
+    get_all_courses()
